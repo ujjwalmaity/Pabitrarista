4d084ea978c5461ab3afd8ed0abd11e3e96a0889
ujjwal <ujjwalmaity98@gmail.com>
2019-08-29 17:26:51 +0530

    Design Login Page

--- app/src/main/java/com/pabitrarista/pabitrarista/RegisterActivity.java
+++ app/src/main/java/com/pabitrarista/pabitrarista/RegisterActivity.java
@@ -2,13 +2,33 @@ package com.pabitrarista.pabitrarista;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    TextView login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        init();
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent in = new Intent(RegisterActivity.this, LoginActivity.class);
+                startActivity(in);
+                finish();
+            }
+        });
+    }
+
+    private void init() {
+        login = findViewById(R.id.register_login);
     }
 }

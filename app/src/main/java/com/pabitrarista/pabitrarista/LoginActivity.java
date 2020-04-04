38095c7805b24c59d7e8657dfd5f08a285126e66
ujjwal <ujjwalmaity98@gmail.com>
2020-04-04 21:07:59 +0530

    Explicit intent

--- app/src/main/java/com/pabitrarista/pabitrarista/LoginActivity.java
+++ app/src/main/java/com/pabitrarista/pabitrarista/LoginActivity.java
@@ -2,7 +2,9 @@ package com.pabitrarista.pabitrarista;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -10,5 +12,14 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        findViewById(R.id.login_free_register).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent in = new Intent(LoginActivity.this, RegisterActivity.class);
+                startActivity(in);
+                finish();
+            }
+        });
     }
 }
